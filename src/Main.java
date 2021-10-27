8f873d9a68afacad664453cb91bbca1cd20e376e
PillProductions <lukas.c.l.nielsen@gmail.com>
2021-10-28 01:15:24 +0200

    Added viewHistory method Fixed custom time option when creating order Added statistic method

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 import javax.swing.*;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 import java.sql.Timestamp;
 import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
@@ -23,28 +26,17 @@ public class Main {
         if (select == 1){
             viewActiveOrders();
         }else if (select == 2){
-            System.out.println("Tryk 1 for at lave en ny ordre\nTryk 2 for at redigere en eksisterende ordre");
-            int select1 = Integer.parseInt(input.nextLine());
-            if (select1 == 1){
-
-                createPizza(input);
-            }if(select1 == 2){
-                editOrder(input);
-            }
-            //Call edit order method
+            editOrder(input);
         }else if (select == 3){
-            //Call order history
+            viewOrderHistory();
         }else if (select == 4){
-            //Call order statistics
-        }else if(select == 5){
-            //CreateOrder.createPizza(input);
-        }
-        if (select != 6){
+            generateStatistics();
+        }if (select != 5){
             mainMenu();
         }
     }
 
-    public static void createPizza(Scanner scan) {
+    public static void createPizza(Scanner scan) throws ParseException {
         Pizza[] currentOrder = new Pizza[1];
         boolean orderFinished = false;
         int finalPrice = 0;
@@ -84,21 +76,43 @@ public class Main {
         System.out.println("Hvad hedder kunden?:");
         recipientName = scan.nextLine();
         System.out.println("Hvornår skal pizzaen hentes?\n1: Om et kvarter\n2: Specielt tidspunkt");
-        if (scan.nextInt() == 2) {
-            Date dateCurrent = new Date();
-            pickupTime = new Timestamp(dateCurrent.getTime());
+        if (scan.nextInt() == 2) { //Shitty code to convert string to date in case cx wants custom pickup time
+            scan.nextLine();
+            System.out.println("Indtast tidspunkt i følgende format: HH:MM (Eks. 18:30)");
+            SimpleDateFormat formatter1 = new SimpleDateFormat("yyyy-MM-dd");
+            Date today = new Date();
+            String inputDate = (formatter1.format(today) + " " + scan.nextLine() + ":" + randomNum(10,59) + "." + randomNum(100,999));
+            SimpleDateFormat formatter2 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+            Date parsed = formatter2.parse(inputDate);
+            pickupTime = new java.sql.Timestamp(parsed.getTime());
         }
         myOrder.newOrder(currentOrder, finalPrice, recipientName, pickupTime);
-        System.out.println("\n*** Bestilling tilføjet til " + pickupTime.toString() + " ***\n");
-        recipientName = scan.nextLine();
+        System.out.println("\n*** Bestilling tilføjet til " + pickupTime.toString().substring(0,16) + " ***\n");
     }
-    public static void editOrder(Scanner scan){
-        System.out.println("Hvilken ordre vil du gerne redigere?");
+    public static void editOrder(Scanner scan) throws Exception {
+        System.out.println("1: Lav en ny ordre\n2: afslut og fjern en eksisterende ordre\n3: Tilbage");
+        int select1 = Integer.parseInt(input.nextLine());
+        if (select1 == 1){
+            createPizza(input);
+        }if(select1 == 2){
+            endOrder(input);
+        }
     }
+    public static void endOrder(Scanner scan) throws Exception {
+        System.out.println("\nHvilken ordre vil du gerne afslutte?: \n");
+        for (int i = 0; i<=myOrder.getActiveOrders().length -2; i++){
+            System.out.println((i+1) + ": " + myOrder.getActiveOrders()[i].getRecipientName().toUpperCase() + " - AFHENTES: " + myOrder.getActiveOrders()[i].getPickupTime().toString() + " - TOTAL PRIS: " + myOrder.getActiveOrders()[i].getFinalPrice());
+        }
+        System.out.print("\nIndtast nummer: ");
+        myOrder.removeOrder(myOrder.getActiveOrders()[(Integer.parseInt(scan.nextLine())-1)]);
+        System.out.println("\n*** Ordre fjernet ***\n");
+        editOrder(input);
+    }
+
 
     public static void viewActiveOrders() throws Exception {
         KeyEvent.exitLoop = false;
-        JFrame jf=new JFrame("Key listener");
+        JFrame jf=new JFrame("Key listener"); //Creating semi-invisible JFrame to detect keystrokes (NEEDS TO BE IN FOCUS)
         jf.setVisible(true);
         jf.setSize(0,0);
         jf.addKeyListener(new KeyEvent());
@@ -107,14 +121,11 @@ public class Main {
             wait(500);
             timer++;
             if (timer==30) {
-                //First we read active orders
-                myOrder.readActiveOrders();
-
-                //Then we print
+                Order[] activeOrders = myOrder.getActiveOrders(); //Gets active orders as Order[] array named activeOrders
                 System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-                for (int i = 0; i <= myOrder.getActiveOrders().length - 2; i++) {
-                    System.out.println(myOrder.getActiveOrders()[i].getRecipientName().toUpperCase() + " - AFHENTES: " + myOrder.getActiveOrders()[i].getPickupTime().toString() + " - TOTAL PRIS: " + myOrder.getActiveOrders()[i].getFinalPrice());
-                    Pizza[] pizzaInOrder = myOrder.getActiveOrders()[i].getPizzaArray();
+                for (int i = 0; i <= activeOrders.length - 2; i++) {
+                    System.out.println(activeOrders[i].getRecipientName().toUpperCase() + " - AFHENTES: " + activeOrders[i].getPickupTime().toString() + " - TOTAL PRIS: " + activeOrders[i].getFinalPrice());
+                    Pizza[] pizzaInOrder = activeOrders[i].getPizzaArray();
                     for (int o = 0; o <= pizzaInOrder.length - 2; o++) {
                         System.out.printf("%-50s %100s %n", pizzaInOrder[o].getNumber() + ". " + pizzaInOrder[o].getName() + " " + pizzaInOrder[o].getComments().toUpperCase(), pizzaInOrder[o].getIngredients() + " - PRIS: " + pizzaInOrder[o].getPrice() + ".-");
                     }
@@ -127,11 +138,54 @@ public class Main {
         }
     }
 
-    public static void wait(int ms){ //Pauses program briefly to make it seem more natural
-        try { //Thread needs to be able to catch exeption for some reason. Idk why
+    public static void viewOrderHistory() throws Exception {
+        System.out.println();
+        Order[] orderHistory = myOrder.getHistoricalOrders(); //Gets active orders as Order[] array named activeOrders
+        for (int i = 0; i <= orderHistory.length - 2; i++) {
+            System.out.println(orderHistory[i].getRecipientName().toUpperCase() + " - AFHENTES: " + orderHistory[i].getPickupTime().toString() + " - TOTAL PRIS: " + orderHistory[i].getFinalPrice());
+            Pizza[] pizzaInOrder = orderHistory[i].getPizzaArray();
+            for (int o = 0; o <= pizzaInOrder.length - 2; o++) {
+                System.out.printf("%-50s %100s %n", pizzaInOrder[o].getNumber() + ". " + pizzaInOrder[o].getName() + " " + pizzaInOrder[o].getComments().toUpperCase(), pizzaInOrder[o].getIngredients() + " - PRIS: " + pizzaInOrder[o].getPrice() + ".-");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void generateStatistics() throws Exception {
+        System.out.println("\n\n*** GENERERER STATESTIK ***\n");
+        Order[] HistoricalOrder = myOrder.getHistoricalOrders();
+        int intArray[] = new int[Menu.list.length]; //Keeps score per sale of each pizza on menu
+        int max = 0; //Keeps track of highest score
+
+        for (int i=0; i<=HistoricalOrder.length-2; i++){ //We start by looping through historical orders
+            for (int o=0; o<=HistoricalOrder[i].getPizzaArray().length-2; o++){ //Then we loop through sold pizzas in each order
+                for (int x=1; x<=Menu.list.length+1; x++){ //Then we loop through every possible pizza number on the menu
+                    if (HistoricalOrder[i].getPizzaArray()[o].getNumber()==x){ //Finally, we check if the pizza number is equivalent to the current number x in our for loop
+                        intArray[x-1]++; //If so, our int arrays equivalent item to the menu number gets +1
+                        if(intArray[x-1]>max){max=intArray[x-1];} //Updates max score
+                    }
+                }
+            }
+        }
+        //Now we print the results in a sorted order
+        for (int i=max;i>=0;i--) { //Loops every number from max sales counted to 0
+            for (int o = 0; o <= intArray.length - 2; o++) { //Loops every salescount
+                if(intArray[o]==i) { //Checks if salescount = i (current max sale)
+                    System.out.println((o + 1) + ". " + Menu.list[o].getName() + ": " + intArray[o] + " SOLGT");
+                }
+            }
+        }
+        System.out.println();
+    }
+
+    public static void wait(int ms){
+        try { //Thread needs to be able to catch exception for some reason. Idk why
             Thread.sleep(ms);
         } catch(InterruptedException ex) {
             Thread.currentThread().interrupt();
         }
     }
+
+    public static int randomNum(int min, int max){
+        return(ThreadLocalRandom.current().nextInt(min, max + 1));}
 }
--- src/Order.java
+++ src/Order.java
@@ -10,6 +10,7 @@ public class Order {
     private Timestamp pickupTime;
     private Pizza[] pizzaArray;
     private Order[] outputObj = new Order[1];
+    private Order[] outputHistoryObj = new Order[1];
 
     public Order(String pRecipientName, int pFinalPrice, Timestamp pPickupTime, Pizza[] PizzaArray) {
         this.recipientName = pRecipientName;
@@ -28,8 +29,12 @@ public class Order {
         return pizzaArray;
     }
     public Order[] getActiveOrders() throws Exception {
-        readActiveOrders();
-        return outputObj;
+        readActiveOrders(); //Reads active_orders.txt and converts to array
+        return outputObj; //Returns array
+    }
+    public Order[] getHistoricalOrders() throws Exception {
+        readHistory(); //Reads order_history.txt and converts to array
+        return outputHistoryObj; //Returns array
     }
 
     public void newOrder(Pizza[] orderArray, int finalprice, String recipientName, Timestamp pickupTime) {
@@ -71,7 +76,7 @@ public class Order {
 
 
 
-    public void readActiveOrders()throws Exception  {
+    private void readActiveOrders()throws Exception  {
         int currentline = 0;
         File myObj = new File("active_orders.txt");
         Scanner myReader = new Scanner(myObj);
@@ -90,7 +95,7 @@ public class Order {
                 }
                 list = Arrays.copyOf(list, list.length + 1); //Resize name array by one more
             }
-            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm");
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
             Date parsed = format.parse(fullparts[3]);
             java.sql.Timestamp timestamp = new java.sql.Timestamp(parsed.getTime());
             outputObj[currentline] = new Order(fullparts[0], Integer.parseInt(fullparts[2]), timestamp, list);
@@ -110,5 +115,88 @@ public class Order {
                 }
             }
         }
+    }
+
+    public void removeOrder(Order orderObj){
+        String output = "";
+
+        try {
+            File myObj = new File("active_orders.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String currentline = myReader.nextLine();
+                if (!currentline.contains(orderObj.getPickupTime().toString().substring(0, 18))) {
+                    output += currentline + "\n";
+                } else if(currentline.contains(orderObj.getPickupTime().toString())){
+                    writeToHistory(currentline);
+                }else {
+                    writeToHistory(currentline);
+                }
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("En fejl har opstået (Fejl 3).");
+            e.printStackTrace();
+        }
+       try {
+            FileWriter myWriter = new FileWriter("active_orders.txt");
+            myWriter.write(output);
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("En fejl er opstået (Fejl 4).");
+            e.printStackTrace();
+        }
+    }
+    private static void writeToHistory(String line){
+        String output = "";
+        try {
+            File myObj = new File("order_history.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                    output += myReader.nextLine() + "\n";
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("En fejl har opstået (Fejl 5).");
+            e.printStackTrace();
+        }
+        output+=line;
+        try {
+            FileWriter myWriter = new FileWriter("order_history.txt");
+            myWriter.write(output);
+            myWriter.close();
+        } catch (IOException e) {
+            System.out.println("En fejl er opstået (Fejl 6).");
+            e.printStackTrace();
+        }
+    }
+
+    public void readHistory()throws Exception  {
+        int currentline = 0;
+        File myObj = new File("order_history.txt");
+        Scanner myReader = new Scanner(myObj);
+        outputHistoryObj = new Order[1];
+        while (myReader.hasNextLine()) {
+            Pizza[] list = new Pizza[1];
+            String[] commentparts = null;
+            String[] fullparts = myReader.nextLine().split(" // ");
+            String[] pizzaparts = fullparts[1].split(" , ");
+            for(int i=0; i<=pizzaparts.length-1; i++){
+                if(pizzaparts[i].contains(" & ")){
+                    commentparts = pizzaparts[i].split(" & ");
+                    list[i] = new Pizza(Menu.list[Integer.parseInt(commentparts[0])-1].getName(), Menu.list[Integer.parseInt(commentparts[0])-1].getIngredients(), commentparts[1], Integer.parseInt(commentparts[0]), Menu.list[Integer.parseInt(commentparts[0])-1].getPrice());
+                } else {
+                    list[i] = new Pizza(Menu.list[Integer.parseInt(pizzaparts[i])-1].getName(), Menu.list[Integer.parseInt(pizzaparts[i])-1].getIngredients(), "", Integer.parseInt(pizzaparts[i]), Menu.list[Integer.parseInt(pizzaparts[i])-1].getPrice());
+                }
+                list = Arrays.copyOf(list, list.length + 1); //Resize name array by one more
+            }
+            SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss.SSS");
+            Date parsed = format.parse(fullparts[3]);
+            java.sql.Timestamp timestamp = new java.sql.Timestamp(parsed.getTime());
+            outputHistoryObj[currentline] = new Order(fullparts[0], Integer.parseInt(fullparts[2]), timestamp, list);
+            outputHistoryObj = Arrays.copyOf(outputHistoryObj, outputHistoryObj.length + 1); //Resize name array by one more
+            currentline++;
+        }
+        myReader.close();
     }
 }
